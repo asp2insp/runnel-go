9142853e6c8a08a55951aa4d24b2706bdb7f42d4
Josiah Gaskin <josiahgaskin@gmail.com>
2015-03-31 21:48:02 -0700

    Added header file, allow growing of pagefile

--- runnel.go
+++ runnel.go
@@ -45,10 +45,12 @@ func (stream *TypedStream) insert(data *Typed) {
 // ==================== INPUT ===================
 
 type inputManagerTyped struct {
-	inChannels []<-chan TypedRef
-	streamData mmap.MMap
-	parent     *TypedStream
-	offset     uint64
+	inChannels   []<-chan TypedRef
+	streamData   mmap.MMap
+	streamHeader *StreamHeader
+	parent       *TypedStream
+	offset       uint64
+	file         *os.File
 }
 
 func newInputManagerTyped(parent *TypedStream) *inputManagerTyped {
@@ -56,55 +58,110 @@ func newInputManagerTyped(parent *TypedStream) *inputManagerTyped {
 	ret.parent = parent
 	ret.offset = 0
 	ret.inChannels = make([]<-chan TypedRef, 0, 1)
-	file, err := os.OpenFile(parent.fname(), os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+
+	// Map in the data
+	ret.streamData, ret.file = ret.mmapFile(parent.fname())
+
+	// Map in the header
+	headerMap, _ := ret.mmapFile(parent.fheader())
+	ret.streamHeader = toHeader(headerMap)
+	ret.streamHeader.FileSize = utils.Filesize(ret.file)
+	return ret
+}
+
+func (inputManager *inputManagerTyped) mmapFile(path string) (mmap.MMap, *os.File) {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
 	utils.Check(err)
-	fileInfo, err := file.Stat()
-	utils.Check(err)
-	if fileInfo.Size() == 0 {
+	if utils.Filesize(file) == 0 {
 		err = file.Truncate(int64(os.Getpagesize()))
 		utils.Check(err)
 	}
 	mapData, err := mmap.Map(file, mmap.RDWR, 0)
 	utils.Check(err)
-	ret.streamData = mapData
-	return ret
+	return mapData, file
 }
 
-func (inputManager *inputManagerTyped) insert(data *Typed) {
-	address := &inputManager.streamData[inputManager.offset]
+func (iM *inputManagerTyped) insert(data *Typed) {
+	header := iM.streamHeader
+	// Check to see if we need to resize
+	if header.EntrySize*(header.EntryCount+1) >= header.FileSize {
+		iM.setFileSizeTo(header.FileSize * 2)
+	}
+
+	address := &iM.streamData[iM.offset]
 	pointer := (*Typed)(unsafe.Pointer(address))
 	*pointer = *data
-	inputManager.offset += uint64(unsafe.Sizeof(*data))
-	inputManager.parent.Size += 1
+	iM.offset += uint64(unsafe.Sizeof(*data))
+	iM.parent.Size += 1
+
+	// Update the header
+	if iM.streamHeader.EntrySize == 0 {
+		iM.streamHeader.EntrySize = uint64(unsafe.Sizeof(*data))
+	}
+	iM.streamHeader.EntryCount += 1
+}
+
+func (inputManager *inputManagerTyped) setFileSizeTo(size uint64) {
+	err := inputManager.file.Truncate(int64(size))
+	utils.Check(err)
+
+	// Re-map our data
+	inputManager.streamData.Unmap()
+	inputManager.streamData, _ = inputManager.mmapFile(inputManager.file.Name())
+
+	// Update the header
+	inputManager.streamHeader.FileSize = size
 }
 
 // =================== OUTPUT ===================
 
 type outputManagerTyped struct {
-	outChannels []chan<- TypedRef
-	streamData  mmap.MMap
-	parent      *TypedStream
+	outChannels   []chan<- TypedRef
+	streamData    mmap.MMap
+	streamHeader  *StreamHeader
+	parent        *TypedStream
+	file          *os.File
+	lastKnownSize uint64
 }
 
 func newOutputManagerTyped(parent *TypedStream) *outputManagerTyped {
 	ret := new(outputManagerTyped)
 	ret.parent = parent
 	ret.outChannels = make([]chan<- TypedRef, 0, 1)
-	file, err := os.Open(parent.fname())
-	utils.Check(err)
-	mapData, err := mmap.Map(file, mmap.RDONLY, 0)
-	utils.Check(err)
-	ret.streamData = mapData
+
+	// Map in the data
+	ret.streamData, ret.file = ret.mmapFile(parent.fname())
+
+	// Map in the header
+	headerMap, _ := ret.mmapFile(parent.fheader())
+	ret.streamHeader = toHeader(headerMap)
+	ret.lastKnownSize = ret.streamHeader.FileSize
 	return ret
 }
 
-func (outputManager *outputManagerTyped) resolve(ref *TypedRef) *Typed {
-	if ref.fileId == outputManager.parent.fileId {
-		address := &outputManager.streamData[ref.offset]
+func (outputManager *outputManagerTyped) mmapFile(path string) (mmap.MMap, *os.File) {
+	file, err := os.Open(path)
+	utils.Check(err)
+	mapData, err := mmap.Map(file, mmap.RDONLY, 0)
+	utils.Check(err)
+	return mapData, file
+}
+
+func (oM *outputManagerTyped) resolve(ref *TypedRef) *Typed {
+	if ref.fileId == oM.parent.fileId {
+		if oM.lastKnownSize != oM.streamHeader.FileSize {
+			oM.refreshMap()
+		}
+		address := &oM.streamData[ref.offset]
 		return (*Typed)(unsafe.Pointer(address))
 	} else {
 		return nil
 	}
+}
+
+func (oM *outputManagerTyped) refreshMap() {
+	oM.streamData.Unmap()
+	oM.streamData, _ = oM.mmapFile(oM.file.Name())
 }
 
 // =================== FILTERS ==================
@@ -115,4 +172,8 @@ func (outputManager *outputManagerTyped) resolve(ref *TypedRef) *Typed {
 
 func (p *TypedStream) fname() string {
 	return filepath.Join(os.TempDir(), p.fileId)
+}
+
+func (p *TypedStream) fheader() string {
+	return filepath.Join(os.TempDir(), p.fileId+"_header")
 }
--- runnel_test.go
+++ runnel_test.go
@@ -2,6 +2,7 @@ package runnel
 
 import "testing"
 import "github.com/asp2insp/go-misc/testutils"
+import "os"
 
 //go:generate genny -in=runnel.go -out=IntStream.go gen "Typed=int"
 
@@ -15,6 +16,33 @@ func TestInsertIncrementsSize(t *testing.T) {
 	testutils.CheckUint64(2, stream.Size, t)
 }
 
+func TestInsertUpdatesInputHeader(t *testing.T) {
+	stream := NewIntStream("test")
+	data1 := 45
+	data2 := 66
+	stream.insert(&data1)
+	stream.insert(&data2)
+
+	testutils.CheckUint64(2, stream.in.streamHeader.EntryCount, t)
+	testutils.CheckUint64(8, stream.in.streamHeader.EntrySize, t)
+}
+
+func TestInsertUpdatesOutputHeader(t *testing.T) {
+	stream := NewIntStream("test")
+	data1 := 45
+	data2 := 66
+	stream.insert(&data1)
+	stream.insert(&data2)
+
+	testutils.CheckUint64(2, stream.out.streamHeader.EntryCount, t)
+	testutils.CheckUint64(8, stream.out.streamHeader.EntrySize, t)
+}
+
+func TestCreateSetsHeaderSize(t *testing.T) {
+	stream := NewIntStream("test")
+	testutils.CheckUint64(uint64(os.Getpagesize()), stream.in.streamHeader.FileSize, t)
+}
+
 func TestRoundTripData(t *testing.T) {
 	stream := NewIntStream("test")
 	for i := 0; i < 100; i++ {
@@ -26,4 +54,22 @@ func TestRoundTripData(t *testing.T) {
 		ref = &IntRef{stream.fileId, uint64(i * 8)}
 		testutils.CheckInt(i, *stream.out.resolve(ref), t)
 	}
+}
+
+func TestPageIncrement(t *testing.T) {
+	stream := NewIntStream("large")
+	// 4096 / 8 = 512
+	for i := 0; i < 513; i++ {
+		stream.insert(&i)
+	}
+	// Should have overflowed onto another page
+	testutils.CheckUint64(uint64(os.Getpagesize()*2), stream.in.streamHeader.FileSize, t)
+
+	var ref *IntRef
+	for i := 0; i < 513; i++ {
+		ref = &IntRef{stream.fileId, uint64(i * 8)}
+		testutils.CheckInt(i, *stream.out.resolve(ref), t)
+	}
+	// Should see output header updated
+	testutils.CheckUint64(uint64(os.Getpagesize()*2), stream.out.streamHeader.FileSize, t)
 }
